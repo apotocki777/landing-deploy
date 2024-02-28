bdc23b1f112beacac616ef189252bbbeeda30212
Alexander Potocki <apotocki@griddynamics.com>
2024-02-28 15:17:45 +0100

    Create a word counter for text area

--- src/form-logic.js
+++ src/form-logic.js
@@ -3,7 +3,10 @@ const selectHeader = document.querySelector(".select_placeholder")
 const placeholderText = document.querySelector(".select_placeholder--text")
 const allSelectValues = document.querySelectorAll(".select-value")
 
+const textArea = document.querySelector("#message")
+const wordCounter = document.querySelector(".text-counter")
 
+// CUSTOM SELECT INPUT
 const changeSelection = (e) => {
     selectedText = e.currentTarget.textContent;
     placeholderText.textContent = selectedText;
@@ -26,4 +29,11 @@ allSelectValues.forEach(value => {
     })
 })
 
+// TEXT AREA WORD COUNTER
 
+const updateTextCounter = () => {
+    let textLength = textArea.value.length
+    wordCounter.textContent = `${textLength}/500`
+}
+
+textArea.addEventListener("keyup", updateTextCounter)
