55f47a95437fc4245b229f8b6c7ee4022ad69f77
Alexander Potocki <apotocki@griddynamics.com>
2024-02-29 15:10:03 +0100

    Fixed sections alignment + featured house slider

--- src/featured-house-slider.js
+++ src/featured-house-slider.js
@@ -3,6 +3,7 @@ const cardsContainer = document.querySelector(".featured-house_cards--wrapper")
 const allCards = document.querySelectorAll(".featured-house_cards--container")
 const slideRightButton = document.querySelector(".arrow-left")
 const slideLeftButton = document.querySelector(".arrow-right")
+const viewportWidth = window.innerWidth
 
 
 
@@ -12,14 +13,20 @@ let transformAmount = 0
 function slideLeft () {
     const frameWidth = frame.offsetWidth;
     const cardWidth = allCards[0].offsetWidth;
+    let gap = 16;
+    let gapPercentage = 8 / viewportWidth * 100
+    console.log(gapPercentage)
 
-    if (slideCount === allCards.length - (Math.round(frameWidth / cardWidth))) return
+    if (slideCount === allCards.length - (Math.round(frameWidth / cardWidth))) {
+        console.log(allCards.length - (Math.round(frameWidth / cardWidth)))
+        return
+    }
 
     transformAmount += 100
     slideCount += 1
 
     allCards.forEach(card => {
-        card.style.transform = `translateX(-${transformAmount}%)`
+        card.style.transform = `translateX(-${transformAmount + 14}%)`
     })
 }
 
